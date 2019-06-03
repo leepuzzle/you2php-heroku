3f3c816bfb8d57ca0569ccd1dbc341b0af26b9e3
leepuzzle <51322447+leepuzzle@users.noreply.github.com>
2019-06-03 21:36:45 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCTAV47h3VZZ5XJqIwd21aqiOEW_endDZE'("APIKEY"));
 define('GJ_CODE', 'SE'("GJ_CODE"));
-define('SITE_NAME','MYU2BE'("SITE_NAME"));
-define('TITLENAME','MYU2BE'("TITLENAME"));
+define('SITE_NAME','myu2be'("SITE_NAME"));
+define('TITLENAME','myu2be'("TITLENAME"));
 define('EN2DEKEY','MY&#$^@GDJJCHjU2BE'("EN2DEKEY"));
 define('EMAIL', 'leepuzzle20@gmail.com'("EMAIL"));
 ?>
